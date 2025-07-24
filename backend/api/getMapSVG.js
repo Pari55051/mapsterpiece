25de53e17a477f67dc6f2c748a3cea665c4def11
Pari55051 <xpari5505@gmail.com>
2025-07-24 16:15:24 +0530

    update map colors #4

--- backend/api/getMapSVG.js
+++ backend/api/getMapSVG.js
@@ -21,9 +21,9 @@ export default async function handler(req, res) {
 
     // Determine theme
     const theme = req.query.theme === 'dark' ? 'dark' : 'light';
-    const backgroundColor = theme === 'dark' ? '#006affff' : '#fefefe';
-    const defaultFill = theme === 'dark' ? '#00ff48ff' : '#cdcdcdff';
-    const defaultStroke = theme === 'dark' ? '#ff0000ff' : '#cdcdcdff';
+    const backgroundColor = theme === 'dark' ? '#171e29ff' : '#fefefe';
+    const defaultFill = theme === 'dark' ? '#616161ff' : '#cdcdcdff';
+    const defaultStroke = theme === 'dark' ? '#616161ff' : '#cdcdcdff';
 
     $('svg').attr('style', `background-color: ${backgroundColor};`);
 
